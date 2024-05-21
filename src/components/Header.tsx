e6932a4eb6228507bf231b54257491568b596535
diipacheco <edilsonpacheco6@gmail.com>
2024-05-21 18:10:44 -0300

    Adds Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import Logo from '../assets/Logo.svg';
+import styles from './Header.module.css';
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <img src={Logo} alt='Logotipo da marca TODO'/>
+    </header>
+  )
+}
